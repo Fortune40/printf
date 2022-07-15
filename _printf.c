dbdda2d08652e2059874b993f0157d33d28e7674
Michael Dankyira <kdankyira.education@gmail.com>
2022-07-15 14:16:20 -0700

    Update files

--- _printf.c
+++ _printf.c
@@ -1,68 +1,51 @@
 #include "main.h"
+
 /**
- * _printf - Entry point
- * Desc: Entry
- *@format: pointer
- * Return: On success.
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
+ *
+ * Return: number of chars printed.
  */
 int _printf(const char *format, ...)
 {
-va_list mylist;
-unsigned int i = 0, j = 0;
+	unsigned int i = 0, len = 0, ibuf = 0;
+	va_list arguments;
+	int (*function)(va_list, char *, unsigned int);
+	char *buffer;
 
-if (!format || (format[0] == '%' && format[1] == '\0'))
-return (-1);
-
-va_start(mylist, format);
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] == '%')
-{
-if (format[i + 1] == '%')
-{_putchar('%');
-j++;
-i++;
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+				return (-1);
+			}
+			else
+			{	function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handl_buf(buffer, format[i], ibuf), len++, i--;
+				}
+				else
+				{
+					len += function(arguments, buffer, ibuf);
+					i += ev_print_func(format, i + 1);
+				}
+			} i++;
+		}
+		else
+			handl_buf(buffer, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
+	}
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	return (len);
 }
-else if (get_op_func(format, i + 1) != NULL)
-{j += (get_op_func(format, i + 1))(mylist);
-i++;
-}
-else
-{_putchar(format[i]);
-j++;
-}
-}
-else
-{_putchar(format[i]);
-j++;
-}
-}
-va_end(mylist);
-return (j);
-}
-/**
- * get_op_func - Entry function
- * @s: operator
- * @pos: position
- * Return: function
- */
-int (*get_op_func(const char *s, int pos))(va_list)
-{
-print_fun ops[] = {
-{"c", print_single_char},
-{"s", print_string_char},
-{"d", print_decimal},
-{"i", print_decimal},
-{NULL, NULL}};
-
-int k;
-for (k = 0; ops[k].op != NULL; k++)
-{
-if (ops[k].op[0] == s[pos])
-{
-return (ops[k].f);
-}
-}
-return (NULL);
-}
-
